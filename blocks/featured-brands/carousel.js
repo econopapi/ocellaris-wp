543e124914767487003738f4e8c8782a4836ce05
Daniel Limón <dlimon.munoz@gmail.com>
2026-02-23 09:59:56 -0600

    [fix]-[featured-products-block] Se corrige render de carrusel en pantalla vertical

--- blocks/featured-brands/carousel.js
+++ blocks/featured-brands/carousel.js
@@ -19,34 +19,55 @@
       var totalItems = $items.length;
       var isAnimating = false;
       var autoplayInterval;
-
-      // Clone items for infinite loop
-      $items.slice(0, itemsToShow).clone().addClass('cloned').appendTo($container);
-      $items.slice(-itemsToShow).clone().addClass('cloned').prependTo($container);
-      
-      var $allItems = $container.find('.brand-item');
       var itemWidth = 100 / itemsToShow;
-      
-      // Set initial position
-      $allItems.css('width', itemWidth + '%');
-      currentIndex = itemsToShow;
-      updateCarousel(false);
+
+      function getItemsToShow() {
+        var windowWidth = $(window).width();
+        if (windowWidth < 480) {
+          return 2;
+        }
+        if (windowWidth < 768) {
+          return 3;
+        }
+        if (windowWidth < 1024) {
+          return 4;
+        }
+        return 5;
+      }
+
+      function rebuildCarousel() {
+        $container.find('.brand-item.cloned').remove();
+
+        if (totalItems > 0) {
+          var cloneCount = Math.min(itemsToShow, totalItems);
+          $items.slice(0, cloneCount).clone().addClass('cloned').appendTo($container);
+          $items.slice(-cloneCount).clone().addClass('cloned').prependTo($container);
+        }
+
+        var $allItems = $container.find('.brand-item');
+        itemWidth = 100 / itemsToShow;
+        $allItems.css('width', itemWidth + '%');
+
+        currentIndex = totalItems > 0 ? itemsToShow : 0;
+        updateCarousel(false);
+      }
+
+      // Set responsive layout and build initial loop structure
+      itemsToShow = getItemsToShow();
+      rebuildCarousel();
 
       // Responsive adjustments
       function updateItemsToShow() {
-        var windowWidth = $(window).width();
-        if (windowWidth < 480) {
-          itemsToShow = 2;
-        } else if (windowWidth < 768) {
-          itemsToShow = 3;
-        } else if (windowWidth < 1024) {
-          itemsToShow = 4;
+        var nextItemsToShow = getItemsToShow();
+
+        if (nextItemsToShow !== itemsToShow) {
+          itemsToShow = nextItemsToShow;
+          rebuildCarousel();
         } else {
-          itemsToShow = 5;
+          itemWidth = 100 / itemsToShow;
+          $container.find('.brand-item').css('width', itemWidth + '%');
+          updateCarousel(false);
         }
-        itemWidth = 100 / itemsToShow;
-        $allItems.css('width', itemWidth + '%');
-        updateCarousel(false);
       }
 
       function updateCarousel(animate) {
